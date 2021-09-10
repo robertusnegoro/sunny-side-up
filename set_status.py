64097404df096d430335f54010d1c07d6adc895d
Robertus Negoro <895404+robertusnegoro@users.noreply.github.com>
2021-09-10 22:33:53 +0700

    add away feature, log to file

--- set_status.py
+++ set_status.py
@@ -1,9 +1,10 @@
 import argparse
 import logging
-
+import os
 from slack_bolt import App
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+log_path = os.getenv('SUNNY_LOG') or '/var/log/sunny.log'
+logging.basicConfig(filename=log_path, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 logger = logging.getLogger(__name__)
 app = App()
@@ -38,16 +39,33 @@ def change_dnd(duration):
         logger.error(e)
         return False
 
+def toggle_away(away):
+    try:
+        if away:
+            away_status = 'away'
+        else:
+            away_status = 'auto'
+            
+        app.client.users_setPresence(presence = away_status)
+        logger.info('updated set to %s' % (away_status))
+        return True
+    except Exception as e:
+        logger.error(e)
+        return False
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--emoji", help="status emoji", default="hammer_and_wrench")
     parser.add_argument("-s", "--status", help="status text", default="")
     parser.add_argument("-d", "--dnd", help="duration DnD", type=int, default=0)
+    parser.add_argument('-a', dest='away', help='away or active', action='store_true')
     args = parser.parse_args()
     arg_status_text = str(args.status)
     arg_status_emoji = str(args.emoji)
     arg_dnd = args.dnd
+    arg_away = args.away
 
     change_profile(arg_status_text, arg_status_emoji)
+    toggle_away(arg_away)
     if arg_dnd > 0:
         change_dnd(arg_dnd)
